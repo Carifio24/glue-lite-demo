9b40b63f44be33df2f2d4c11ea7b0d75a38f48c5
Carifio24 <carifio24@gmail.com>
2025-08-25 16:30:17 -0400

    More progress on scripts. Still dealing with pyodide version issues.

---
+++ build.sh
@@ -0,0 +1,8 @@
+cp dist/*.whl pypi/
+
+rm .jupyterlite.doit.db
+rm -rf .cache
+
+rm -rf dist
+jupyter lite build --output-dir dist
+cp pypi/*.whl dist/extensions/@jupyterlite/pyodide-kernel-extension/static/pypi
--- wheels.sh
+++ wheels.sh
@@ -1,16 +1,3 @@
-## Usage
-
-Install dependencies:
-
-```bash
-pip install jupyterlite-core jupyterlite-pyodide-kernel
-pip install jupyter-server jupyterlab-server
-```
-
-Build wheels for fast-histogram and mpl-scatter-density
-```bash
-pip install pyodide-build
-
 # Download emscripten
 rm -rf emsdk
 git clone https://github.com/emscripten-core/emsdk.git
@@ -18,23 +5,9 @@ cd emsdk
 PYODIDE_EMSCRIPTEN_VERSION=$(pyodide config get emscripten_version)
 ./emsdk install ${PYODIDE_EMSCRIPTEN_VERSION}
 ./emsdk activate ${PYODIDE_EMSCRIPTEN_VERSION}
+echo $PWD
 source ./emsdk_env.sh
 
 # Build
 pyodide build https://files.pythonhosted.org/packages/e8/77/04a9b4b5caa6e6b3a2f633b15dec0996c1559fc26e9ba73bb3d1d844c874/fast_histogram-0.14.tar.gz
 pyodide build https://files.pythonhosted.org/packages/f1/8c/f5c1ddc7d35f0337b2698b52e60f558db3bf03e80ec0b13b8f8b9e2cf2e8/mpl_scatter_density-0.8.tar.gz
-```
-
-Build the site:
-
-```bash
-./build.sh
-```
-
-Serve it locally:
-
-```bash
-python -m http.server --directory dist 8000
-```
-
-Visit 👉 http://localhost:8000
