3b2b9259f2240025798a1ff817f124c8e5d9c75c
Carifio24 <carifio24@gmail.com>
2025-08-26 13:03:29 -0400

    Update build script and JSON config.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm .jupyterlite.doit.db
 rm -rf .cache
 
 rm -rf dist
-jupyter lite build --output-dir dist
+jupyter lite build --debug --config jupyter-lite.json --output-dir dist
 cp pypi/*.whl dist/extensions/@jupyterlite/pyodide-kernel-extension/static/pypi
