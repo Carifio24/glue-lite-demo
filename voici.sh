8272c5508015b038a52b38a4c20dc69e744d6f9e
Carifio24 <carifio24@gmail.com>
2025-09-04 15:59:18 -0400

    Update voici build instruction. Tinker with requirements.

--- voici.sh
+++ voici.sh
@@ -1,4 +1,4 @@
 rm -rf _output
 
-voici build --contents content content/glue-lite.ipynb
+voici build --contents content content/glue-lite.ipynb --output-dir dist
 cp pypi/*.whl _output/extensions/@jupyterlite/pyodide-kernel-extension/static/pypi
